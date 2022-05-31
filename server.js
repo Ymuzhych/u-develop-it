e8b1ef04980500805055745f8c9800abd0074fdc
Ymuzhych <myuliia2018@gmail.com>
2022-05-31 13:48:00 -0500

    first database calls

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mysql = require('mysql2');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -6,15 +8,26 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Hello World'
-  });
-});
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: 'Khadka0529&',
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
+);
 // Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
